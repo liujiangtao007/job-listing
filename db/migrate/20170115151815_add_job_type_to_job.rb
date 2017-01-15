36ed058d60d6979673481eb5057d00da40988f6c
liujiangtao <liujiangtao007@gmail.com>
2017-01-15 23:50:25 +0800

    add job type

---
+++ db/migrate/20170115151815_add_job_type_to_job.rb
@@ -0,0 +1,5 @@
+class AddJobTypeToJob < ActiveRecord::Migration[5.0]
+  def change
+    add_column :jobs, :job_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115075551) do
+ActiveRecord::Schema.define(version: 20170115151815) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170115075551) do
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
     t.boolean  "is_hidden",        default: true
+    t.string   "job_type"
   end
 
   create_table "resumes", force: :cascade do |t|
